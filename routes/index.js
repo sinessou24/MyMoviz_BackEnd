d75e4c04b5d902c91e0849e6b32a93388cd80753
sinessou <sinessou@gmail.com>
2024-02-07 16:10:07 +0100

    first commit

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,7 @@ router.get('/movies', (req, res) => {
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`)
     .then(response => response.json())
     .then(data => {
-        res.json({movies: data.results, key: API_KEY});
+        res.json({movies: data.results});
     })
 
 })
